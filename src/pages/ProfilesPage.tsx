71f567b67a2c1abf46c340865a1c60ff4b49bd46
ZonRyu <julyonchang@gmail.com>
2024-09-12 17:23:09 +0700

    NavLink, Outlet

--- src/pages/ProfilesPage.tsx
+++ src/pages/ProfilesPage.tsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 const ProfilesPage = () => {
   const profiles = [1, 2, 3, 4, 5]
 
   return (
-    <div className="flex flex-col gap-2">
-        {profiles.map((profile) => (
-          <Link to={`/profile/${profile}`} key={profile} >
-            Profile {profile}
-          </Link>
-        ))}
+    <div className="flex gap-2">
+        <div className="flex flex-col gap-2">
+            {profiles.map((profile) => (
+            <NavLink 
+                to={`/profiles/${profile}`} 
+                key={profile} 
+                className={({ isActive }) => {
+                    return isActive ? "bg-slate-300" : ""
+                }}
+                // NavLink provide a `className` prop that can be used to style the link
+                // The `isActive` prop will be true when the link is active
+            >
+                Profile {profile}
+            </NavLink>
+            ))}
+        </div>
+        <Outlet /> {/* To show the content of the child route */}
     </div>
   )
 }
